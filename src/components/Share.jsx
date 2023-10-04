0efaac5f3acdf71b1071d237b5f0dd8a3ef8f780
TheRitwik <ritwikkumar.satpati@gmail.com>
2023-10-04 20:09:26 +0530

    testing - message: text

--- src/components/Share.jsx
+++ src/components/Share.jsx
@@ -12,8 +12,6 @@ const Share = () => {
         const text = "Web Share API in ReactJS Example in Github"
         const url = "https://github.com/ritwik-satpati/web-share-api-in-reactjs"
 
-        console.log(`${text} - ${url}`)
-
         // Load the image and convert it to a Blob
         const response = await fetch(StoryImage);
         const blob = await response.blob();
@@ -28,20 +26,22 @@ const Share = () => {
             text: text,
             url: url,
             files: files,
+            message: text,
         }
         // Sharing the Share-Dataset with Files
+
         await navigator.share(shareData)
+        console.log("Shared without Files!")
     }
 
 
-
-return (
-    <div className='h-[100vh] w-full flex items-center justify-center'>
-        <div className='h-[40px] min-w-[50px] flex items-center justify-center space-x-2 border border-blue-600 text-blue-600 hover:text-white hover:bg-blue-600 font-Roboto text-center cursor-pointer py-2 px-5 rounded-sm' onClick={handleShare}>
-            Share
+    return (
+        <div className='h-[100vh] w-full flex items-center justify-center'>
+            <div className='h-[40px] min-w-[50px] flex items-center justify-center space-x-2 border border-blue-600 text-blue-600 hover:text-white hover:bg-blue-600 font-Roboto text-center cursor-pointer py-2 px-5 rounded-sm' onClick={handleShare}>
+                Share
+            </div>
         </div>
-    </div>
-)
+    )
 
 }
 
