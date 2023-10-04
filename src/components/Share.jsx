c2b7da65a2338f3f3f40833291a97008084b57db
TheRitwik <ritwikkumar.satpati@gmail.com>
2023-10-04 21:36:40 +0530

    testing - changing in shareData order - title >> files >> textWithUrl

--- src/components/Share.jsx
+++ src/components/Share.jsx
@@ -7,7 +7,7 @@ const Share = () => {
 
         console.log("Share button clicked!")
 
-        // const title = "Web Share API"
+        const title = "Web Share API"
         const text = "Web Share API in ReactJS Example in Github"
         const url = "https://github.com/ritwik-satpati/web-share-api-in-reactjs"
 
@@ -24,9 +24,9 @@ const Share = () => {
 
         // Setting Share-Dataset with Files 
         const shareData = {
-            // title: title,
+            title: title,
             files: files,
-            url: null,
+            // url: url,
             text: textWithUrl,
             // text: text,
         }
